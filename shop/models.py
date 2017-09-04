af88b9d8a2d638722c5dad459f5dee15922550eb
frank <franciskipchumba5@gmail.com>
2017-09-04 13:36:30 +0300

    building catalog views

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from django.core.urlresolvers import reverse
 
 from django.db import models
 
@@ -15,6 +16,9 @@ class Category (models.Model):
 		verbose_name = 'category'
 		verbose_name_plural = 'categories'
 
+	def get_absolute_url(self):
+		return reverse('shop:product_list_by_category',
+						args=[self.slug])
 	def __str__(self):
 		return self.name
 
@@ -39,6 +43,10 @@ class Product(models.Model):
 	class Meta:
 		ordering = ('name',)
 		index_together = (('id','slug'),)
+
+	def get_absolute_url(self):
+		return reverse('shop:product_detail',
+						args=[self.id, self.slug])
 
 	def __st__(self):
 		return self.name
