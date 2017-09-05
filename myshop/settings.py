ed0c22cedc5e041ba7bf63ec497dd2b96e1b732d
frank <franciskipchumba5@gmail.com>
2017-09-05 12:24:09 +0300

    add shoping cart and sessions

--- myshop/settings.py
+++ myshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# sessions
+CART_SESSION_ID = 'cart'
