3a9d6cd84c2af9075d3d9a0f930651b62c540e5c
frank <franciskipchumba5@gmail.com>
2017-09-09 16:58:33 +0300

    extend admin with custom views

--- orders/views.py
+++ orders/views.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse 
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
 from .tasks import order_created
@@ -36,3 +38,10 @@ def order_create(request):
 	return render(request,
 					'orders/order/create.html',
 					{'cart': cart, 'form': form})
+
+@staff_member_required
+def admin_order_detail(request, oder_id):
+	order = get_object_or_404(Order, id=oder_id)
+	return render(request, 
+					'admin/orders/order/detail.html',
+					{'order': order})
