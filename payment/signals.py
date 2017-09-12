35da86780c927ec65075cf9fb700400a45686a7c
frank <franciskipchumba5@gmail.com>
2017-09-12 16:31:01 +0300

    send pdf by mail

--- payment/signals.py
+++ payment/signals.py
@@ -1,6 +1,14 @@
+import weasyprint 
+
 from django.shortcuts import get_object_or_404
 from paypal.standard.models import ST_PP_COMPLETED
 from paypal.standard.ipn.signals import valid_ipn_received
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+from io import BytesIO
+
 from orders.models import Order
 
 def payment_notification(sender, **kwargs):
@@ -12,6 +20,28 @@ def payment_notification(sender, **kwargs):
 		# mark order as paid
 		order.paid = True
 		order.save()
+
+		# crreate invoice e-mail
+		subject = 'My shop - invoice no. {}'.format(order.id)
+		message = 'please find attached the invoice for yor recent purchase'
+		email = EmailMessage(subject,
+							message,
+							'admin@myshop.com'
+							[order.email])
+		# generate pdf
+		html = render_to_string('oders/order/pdf.html',{'order':order})
+		out= BytesIO()
+		stylesheets = [weasyprint.css(settings.STATIC_ROOT+'css/pdf.css')]
+		weasyprint.HTML(string=html).write_pdf(out,
+												stylesheets=stylesheets)
+		# attach a pdf
+		email.attach('order_{}.pdf'.format(order.id),
+						out.getvalue(),
+						'application/pdf')
+
+		# send email
+		email.send()
+
 
 valid_ipn_received.connect(payment_notification)
 
