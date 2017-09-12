5b7346e40e865cb65c8e51a831b58bb514f0a37a
frank <franciskipchumba5@gmail.com>
2017-09-12 16:14:31 +0300

    render page invoice to pdf

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import weasyprint
+
 from django.shortcuts import render, redirect
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse 
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from cart.cart import Cart
@@ -42,6 +48,17 @@ def order_create(request):
 @staff_member_required
 def admin_order_detail(request, oder_id):
 	order = get_object_or_404(Order, id=oder_id)
+	html = render_to_string('orders/order/pdf.html',{'order':order})
+	response = HttpResponse(content_type='application/pdf')
+	response['Content-Disposition'] - 'filename=\
+			"order_{}.pdf"'.format(order.id)
+	weasyprint.HTML(string=html).write_pdf(reaponse,
+		stylesheets=[weasyprint.css(
+			settings.STATIC_ROOT+'css/pdf.css')])
+	
+	return response
+
+
 	return render(request, 
 					'admin/orders/order/detail.html',
 					{'order': order})
